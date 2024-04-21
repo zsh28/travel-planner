52784135de675e465822a5a4d2ededc43a15d02c
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2024-04-21 14:05:52 +0200

    Replaced firebase account/and fixed added authentication

--- Screens/Login.tsx
+++ Screens/Login.tsx
@@ -1,30 +1,56 @@
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
-import { useContext } from "react";
+import { View, Text, TouchableOpacity, TextInput, Alert } from "react-native";
+import { useContext, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Header, Button } from "../components";
 import ThemeContext from "../theme/ThemeContext";
 import { LightStyles, DarkStyles } from "../Styles";
+import auth from "@react-native-firebase/auth";
 
 const Login = () => {
   const { theme } = useContext(ThemeContext);
   const styles = theme === "light" ? LightStyles : DarkStyles;
+
   const nav = useNavigation();
 
   const handlerRegister = () => {
     nav.navigate("Register" as never);
   };
 
-  const loginFunction = () => {};
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loginFunction = () => {
+    if (!loginForm.email || !loginForm.password || loginForm.email === "" || loginForm.password === "") {
+      Alert.alert("Please enter all fields.");
+      return;
+    };
+
+    auth()
+      .signInWithEmailAndPassword(loginForm.email, loginForm.password)
+      .then(() => {
+        nav.navigate("Home" as never);
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-email") {
+          console.log("That email address is invalid!");
+        }
+        console.error(error);
+      });
+  };
 
   return (
     <View style={styles.container}>
       <TextInput
+        onChangeText={(text) => setLoginForm((f) => ({ ...f, email: text }))}
         style={styles.input}
         placeholder="Email"
         keyboardType="email-address"
         placeholderTextColor={styles.placeholder.color}
       />
       <TextInput
+        onChangeText={(text) => setLoginForm((f) => ({ ...f, password: text }))}
         style={styles.input}
         placeholder="Password"
         secureTextEntry
@@ -32,9 +58,9 @@ const Login = () => {
       />
       <Button
         onPress={loginFunction}
-        disabled={false}
-        variant={"green"}
         text={"Login"}
+        variant={"green"}
+        disabled={false}
       />
       <Button
         onPress={handlerRegister}
